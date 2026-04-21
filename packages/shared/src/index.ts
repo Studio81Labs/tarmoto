1f4c3a726b86ccc18e510e7ed36dd4db8490b61e
Adam Kadlec <adam.kadlec@gmail.com>
2026-04-21 10:14:43 +0200

    feat(shared): add curated region catalog for best-roads pages

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -3,6 +3,7 @@
  * Shared types, constants, and DTOs for Tarmoto.
  */
 
-export * from './constants';
-export * from './geo';
-export * from './units';
+export * from "./constants";
+export * from "./geo";
+export * from "./units";
+export * from "./regions";
