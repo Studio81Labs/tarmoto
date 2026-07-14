b163b3d1bdf172cfb42a920829728b71d7f3f90a
Adam Kadlec <adam.kadlec@gmail.com>
2026-07-14 21:52:15 +0200

    test(cross): drop unused userEvent import in NumberField test

--- packages/ui/src/controls/__tests__/NumberField.test.tsx
+++ packages/ui/src/controls/__tests__/NumberField.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import { NumberField } from "../NumberField";
 
 test("clamps and reports numeric changes", async () => {
