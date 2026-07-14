f413a86fb8ff76da51be176f6f2b106cdf8794be
Adam Kadlec <adam.kadlec@gmail.com>
2026-07-14 21:36:07 +0200

    feat(cross): add FieldLabel + FieldHint primitives to @tarmoto/ui

---
+++ packages/ui/src/controls/field/index.ts
@@ -0,0 +1,3 @@
+export { fieldChrome, type FieldChromeOptions } from "./fieldChrome";
+export { FieldLabel, type FieldLabelProps } from "./FieldLabel";
+export { FieldHint, type FieldHintProps } from "./FieldHint";
